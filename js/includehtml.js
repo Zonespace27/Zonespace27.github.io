3db29d527ab9f01e111cdf8ac12201aaed8c4294
Zonespace <41448081+Zonespace27@users.noreply.github.com>
2023-05-28 22:30:01 -0700

    cuts down on js size, barely

--- js/includehtml.js
+++ js/includehtml.js
@@ -1,25 +1,21 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
+    var z, i, e, f, x;
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
+      e = z[i];
+      f = e.getAttribute("include-html");
+      if (f) {
+        x = new XMLHttpRequest();
+        x.onreadystatechange = function() {
           if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("include-html");
+            if (this.status == 200) {e.innerHTML = this.responseText;}
+            if (this.status == 404) {e.innerHTML = "Page not found.";}
+            e.removeAttribute("include-html");
             includeHTML();
           }
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
+        x.open("GET", file, true);
+        x.send();
         return;
       }
     }
