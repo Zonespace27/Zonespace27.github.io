7b692da6905206b728b86454bbc56e3665628277
John Doe <gamingskeleton3@gmail.com>
2024-01-26 14:57:36 -0800

    prettier + fix

--- js/includehtml.js
+++ js/includehtml.js
@@ -1,22 +1,26 @@
 function includeHTML() {
-    var z, i, e, f, x;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      e = z[i];
-      f = e.getAttribute("include-html");
-      if (f) {
-        x = new XMLHttpRequest();
-        x.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {e.innerHTML = this.responseText;}
-            if (this.status == 404) {e.innerHTML = "Page not found.";}
-            e.removeAttribute("include-html");
-            includeHTML();
+  var z, i, e, f, x;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    e = z[i];
+    f = e.getAttribute("include-html");
+    if (f) {
+      x = new XMLHttpRequest();
+      x.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            e.innerHTML = this.responseText;
           }
+          if (this.status == 404) {
+            e.innerHTML = "Page not found.";
+          }
+          e.removeAttribute("include-html");
+          includeHTML();
         }
-        x.open("GET", f, true);
-        x.send();
-        return;
-      }
+      };
+      x.open("GET", f, true);
+      x.send();
+      return;
     }
   }
+}
